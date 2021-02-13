26a37e4eb25f6bfabed6cdf65a06b021f06c300e
Rafael Romero Lopes <rafarlopes@hotmail.com>
2021-02-13 09:02:51 +0100

    inital structure for the routes endpoint

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func routesHandler(w http.ResponseWriter, r *http.Request) {
+	source, ok := r.URL.Query()["src"]
+	if !ok || len(source) != 1 {
+		handleError(w, http.StatusBadGateway, "one src parameter must be specified")
+		return
+	}
+
+	_, ok = r.URL.Query()["dst"]
+	if !ok {
+		handleError(w, http.StatusBadGateway, "at least one dst parameter must be specified")
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("ok")
+}
+
+func handleError(w http.ResponseWriter, code int, message string) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(message)
+}
 
 func main() {
-	fmt.Println("hello")
+	// TODO handle not found
+	http.HandleFunc("/routes", routesHandler)
+
+	log.Println("starting listener")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	// TODO handle graceful shutdown
 }
